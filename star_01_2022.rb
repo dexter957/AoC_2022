4ea140c4f62c10576f425538d6be5ef12280d77b
Ileanna <ileanna957@gmail.com>
2023-01-22 20:08:53 +0200

    Update star_01_2022.rb

--- star_01_2022.rb
+++ star_01_2022.rb
@@ -1,5 +1,7 @@
  
+ 
 
+#Part 1 of the problem
 
 calFile = "calories.txt"
 file = File.open(calFile,"r")
@@ -23,17 +25,26 @@ contents.each do |content|
 end
 elfCals[i] = sumCals
 
-elfClasSrt = elfCals.sort_by{|k,v| v}.to_h
+elfClasSrt = elfCals.sort_by{|k,v| v}.reverse.to_h
 
-puts "Elf no. #{elfClasSrt.keys.last.to_s} is carrying the most calories (#{elfClasSrt.values.last.to_s})"
-
-
+puts "Elf no. #{elfClasSrt.keys.first.to_s} is carrying the most calories (#{elfClasSrt.values.first.to_s})"
 
 
 
 
+# Bonus part of the problem
 
+puts "Top elfs in carrying calories are:"
+i = 1
+sum = 0
+elfClasSrt.each do |key, value|
+    puts "Elf no. #{key.to_s} is carrying #{value.to_s} calories"
+    i += 1
+    sum += value
+    break if i > 3
+end
 
+puts "And they are carrying #{sum.to_s} calories in total"
 
 
 
